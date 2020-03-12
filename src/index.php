f55e047e6d821d801bc3862b0b07cef2106fa1d2
ogaty <ogaty@example.com>
2020-03-12 22:38:06 +0900

    lp 1

---
+++ src/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <style>
+        .lp {
+            width: 200px;
+        }
+        .wrap {
+            display: flex;
+            margin: 40px;
+        }
+        .wrap__child {
+            width: 30%;
+            text-align: center;
+        }
+        .caption {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>LandingPageサンプル集</h1>
+    <div>
+        適当なテンプレートをHTMLに起こしてみました。<br>
+        すべてレスポンシブ対応です。
+    </div>
+    <div class="wrap">
+        <div class="wrap__child">
+            <a href="/1/index.html"><img src="/1/lp1.jpg" alt="1" class="lp"></a><br>
+            <div class="caption">
+                <a href="/1/index.html">
+                    LP 1
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
