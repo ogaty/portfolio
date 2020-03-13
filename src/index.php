2dc145f479b0cb475a9bc5d40b1f542704c623e0
ogaty <ogaty@example.com>
2020-03-13 10:52:55 +0900

    lp 2

--- src/index.php
+++ src/index.php
@@ -20,12 +20,22 @@
         }
         .caption {
             text-align: center;
+            width: 200px;
+            margin: 0 auto;
+        }
+        @media only screen and (max-width: 450px) {
+            h1 {
+                font-size: 1.1em;
+            }
+            .description {
+                font-size: 0.8rem;
+            }
         }
     </style>
 </head>
 <body>
     <h1>LandingPageサンプル集</h1>
-    <div>
+    <div class="description">
         適当なテンプレートをHTMLに起こしてみました。<br>
         すべてレスポンシブ対応です。
     </div>
@@ -38,6 +48,18 @@
                 </a>
             </div>
         </div>
+        <div class="wrap__child">
+            <a href="/2/index.html"><img src="/2/lp2.jpg" alt="1" class="lp"></a><br>
+            <div class="caption">
+                <a href="/2/index.html">
+                    LP 2
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="description">
+        LP作成、システム作成の依頼は気軽に受け付けております。<br>
+        <a href="https://ogatism.jp/biz">詳細はこちらまで。</a>
     </div>
 </body>
 </html>
